c0cda0407da79a5b777f30d6ef85d6c5127e8a54
OliveiraConstrucao <oliveira1construcao@gmail.com>
2026-05-02 01:21:06 +0000

    limpeza estrutura definitiva

--- src/app/supabaseClient.ts
+++ src/app/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bivmvrgspcjuncjyszsh.supabase.co'
-const supabaseKey = 'sb_publishable_F3Wln1IjUlu2yK0HxHGGxw_rT56ioca'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
